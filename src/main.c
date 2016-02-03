ea08033c9d45c4c3069c8a2e4e22a0333a8e88f1
Ft. Knox <ftknox@riseup.net>
2016-02-03 13:51:27 -0500

    Placeholder code
    Signed-off-by: Ft. Knox <ftknox@riseup.net>

---
+++ src/main.c
@@ -0,0 +1,30 @@
+/**
+ * rosk
+ * A randomizing on screen keyboard using ncurses
+ *
+ * Copyright 2016 Ft. Knox <ftknox@riseup.net>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+#include <config.h>
+#include <stdio.h>
+
+int
+main (void)
+{
+	puts("rosk");
+	puts("This is " PACKAGE_STRING ".");
+	return 0;
+}
